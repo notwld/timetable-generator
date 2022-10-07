dc4a138adcf20b8795ab920a5008d0ce4d31856c
M Waleed <mwfarrukh@gmail.com>
2022-10-07 13:47:44 +0500

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import json
+
+
+app = Flask(__name__)
+data = json.load(open('data.json', 'r'))
+print(data["data"])
+
+@app.route('/')
+def index():
+    return render_template('index.html',data=data["data"])
+
+app.run(debug=True)
