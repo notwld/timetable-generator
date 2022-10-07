f498ce7362bf005a6903fc37ebb6d474e0a74c66
M Waleed <mwfarrukh@gmail.com>
2022-10-07 15:57:50 +0500

    fixed double pages

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template
 import json
-import webbrowser
-from threading import Timer
 
 
 app = Flask(__name__)
@@ -12,5 +10,4 @@ def index():
     return render_template('index.html',data=data["data"])
 
 if __name__ == '__main__':
-    Timer(1, lambda: webbrowser.open('http://localhost:5000/')).start()
     app.run(debug=True)
