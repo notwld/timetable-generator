f2157681c40d9585c77343087078dec1f2220633
M Waleed <mwfarrukh@gmail.com>
2022-10-07 13:59:01 +0500

    initial commit

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
 import json
+import webbrowser
+from threading import Timer
 
 
 app = Flask(__name__)
 data = json.load(open('data.json', 'r'))
-print(data["data"])
 
 @app.route('/')
 def index():
     return render_template('index.html',data=data["data"])
 
-app.run(debug=True)
+if __name__ == '__main__':
+    Timer(1, lambda: webbrowser.open('http://localhost:5000/')).start()
+    app.run(debug=True)
